012c9c1cb8572c9f632c3de0469a33fe6c4c7edc
liyunfengfengfeng <2305743208@qq.com>
2018-03-03 21:55:11 +0800

    优化代码格式增加必要注释

--- src/main/java/com/yingjun/ssm/web/UserController.java
+++ src/main/java/com/yingjun/ssm/web/UserController.java
@@ -4,28 +4,38 @@ package com.yingjun.ssm.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 
 
+/**
+ * 用户
+ * @author 包
+ */
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
 	private final Logger LOG = LoggerFactory.getLogger(this.getClass());
-	
 
-
+    /**
+     * 用户登录
+     * @return
+     */
 	@RequestMapping(value = "/userLogin", method = RequestMethod.GET)
-	public String userLogin(Model model, Integer offset, Integer limit) {
-		LOG.info("invoke----------/user/list");
+	public String userLogin() {
+		LOG.info("用户登录");
 		return "/user/userLogin";
 	}
+
+    /**
+     * 用户注册
+     * @return
+     */
 	@RequestMapping(value = "/userRegister", method = RequestMethod.GET)
-	public String userRegister(Model model, Integer offset, Integer limit) {
-		LOG.info("invoke----------/user/list");
+	public String userRegister() {
+		LOG.info("用户注册");
 		return "/user/userResgiter";
 	}
 }
