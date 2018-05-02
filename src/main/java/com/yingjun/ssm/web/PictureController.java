a8a7a08cfeb9dbed69fe56d600c55947871c0044
liyunfengfengfeng <2305743208@qq.com>
2018-05-02 19:30:33 +0800

    系统logo管理

---
+++ src/main/java/com/yingjun/ssm/web/PictureController.java
@@ -0,0 +1,35 @@
+package com.yingjun.ssm.web;
+
+
+import com.google.gson.Gson;
+import com.yingjun.ssm.dto.BaseResult;
+import com.yingjun.ssm.entity.User;
+import com.yingjun.ssm.enums.ResultEnum;
+import com.yingjun.ssm.exception.BizException;
+import com.yingjun.ssm.service.UserService;
+import com.yingjun.ssm.util.CodeUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+
+/**
+ * 图片管理
+ * @author liyunfeng
+ */
+@Controller
+@RequestMapping("/picture")
+public class PictureController {
+
+	private final Logger LOG = LoggerFactory.getLogger(this.getClass());
+
+
+}
--- src/main/webapp/WEB-INF/jsp/admin/left.jsp
+++ src/main/webapp/WEB-INF/jsp/admin/left.jsp
@@ -56,8 +56,8 @@
         <ul class="menuson">
             <li><a href="/adminUser/toUserOperate" target="rightFrame">用户管理</a></li>
             <li><a href="/adminCompanyManager/toCompanyManager" target="rightFrame">公司管理</a></li>
-            <li><a href="imgtable.jsp" target="rightFrame">图片列表</a></li>
-            <li><a href="imglist.jsp" target="rightFrame">图片网格</a></li>
+            <li><a href="/picture/logoManager" target="rightFrame">logo管理</a></li>
+            <li><a href="imglist.jsp" target="rightFrame">系统名称管理</a></li>
             <li><a href="imglist1.jsp" target="rightFrame">图片自定义</a></li>
             <li><a href="tools.jsp" target="rightFrame">常用工具</a></li>
             <li><a href="filelist.jsp" target="rightFrame">文件管理</a></li>
