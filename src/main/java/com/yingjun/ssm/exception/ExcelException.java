42e9049d5500d34ba4b93327532bf45226ec08dc
liyunfengfengfeng <2305743208@qq.com>
2018-05-10 14:42:52 +0800

    报表导出...

---
+++ src/main/java/com/yingjun/ssm/exception/ExcelException.java
@@ -0,0 +1,28 @@
+package com.yingjun.ssm.exception;
+
+/**
+ * Created by liyunfeng on 2018/5/10.
+ */
+public class ExcelException extends Exception {
+
+    public ExcelException() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public ExcelException(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+    public ExcelException(Throwable cause) {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    public ExcelException(String message, Throwable cause) {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
+
+
+}
